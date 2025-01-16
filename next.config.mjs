ea9d26b3a46c128f3e0b1ffc470864dd1354b0ee
ZubaydullaCoder <zubaydullacoder@gmail.com>
2025-01-16 23:32:17 +0500

    Update next.config.mjs to allow all hostnames for image sources

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["ibb.co", "i.ibb.co", "plus.unsplash.com", "images.unsplash.com"], // Add both domains for ibb.co
+    // domains: ["ibb.co", "i.ibb.co", "plus.unsplash.com", "images.unsplash.com"], // Add both domains for ibb.co
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**.ibb.co",
+        hostname: "**", // This allows all hostnames for HTTPS
+      },
+      {
+        protocol: "http", // If you need HTTP as well (less secure)
+        hostname: "**",
       },
     ],
   },
